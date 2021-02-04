01c9721ba269aa4893378a22c4aa429e1dff224b
Tomasz Pecak <pecatoma@gmail.com>
2021-02-04 11:48:38 +0100

    Create timer and handle timeout

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,12 +2,14 @@ import Grid from "@material-ui/core/Grid";
 import Paper from "@material-ui/core/Paper";
 import {EmojiEvents, Timer} from "@material-ui/icons";
 import {Typography} from "@material-ui/core";
-import React from "react";
+import React, {useEffect, useState} from "react";
 import {createStyles, makeStyles, Theme} from "@material-ui/core/styles";
 import {Logo} from "./common/Logo";
 import clsx from "clsx";
 import {useSelector} from "react-redux";
 import {AppModel} from "../state/AppModel";
+import {useConfigContext} from "../state/ConfigContext";
+import {useModalContext} from "./ModalContexProvider";
 
 const useStyles = makeStyles((theme: Theme) =>
     createStyles({
@@ -34,7 +36,37 @@ const useStyles = makeStyles((theme: Theme) =>
 );
 export default function Header() {
     const classes = useStyles();
-    const {points} = useSelector((state: AppModel) => state)
+    const {points, questionCounter} = useSelector((state: AppModel) => state)
+    const {roundTime} = useConfigContext()
+    const [startTime, setStartTime] = useState<number>(Date.now)
+    const {setMode, setOpen} = useModalContext()
+
+    const calculateTimeLeft = () :number => {
+        const time = Math.round((roundTime - (Date.now() - startTime)) / 1000)
+        return time < 0 ? 0 : time
+    };
+
+    const [timeLeft, setTimeLeft] = useState<number>(roundTime/1000);
+
+    useEffect(() => {
+        setStartTime(Date.now())
+    }, [questionCounter])
+
+    useEffect(() => {
+        const timer=setTimeout(() => {
+            setTimeLeft(calculateTimeLeft());
+        }, 1000);
+        // Clear timeout if the component is unmounted
+        return () => clearTimeout(timer);
+    });
+
+
+    useEffect(() => {
+        if(timeLeft <= 0){
+            setOpen(true)
+            setMode("timeout")
+        }
+    }, [setMode, setOpen, timeLeft]);
 
     return (
         <Grid container justify={"space-between"} spacing={2} className={classes.container}>
@@ -42,7 +74,7 @@ export default function Header() {
                 <Paper elevation={3} className={classes.paper}>
                     <Timer className={classes.icon}/>
                     <Typography>
-                        80s
+                        {timeLeft} s
                     </Typography>
                 </Paper>
             </Grid>
